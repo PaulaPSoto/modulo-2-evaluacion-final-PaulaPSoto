d4088a47e6f367012e4a0524c4d726fce8ff1e0e
PaulaPSoto <paezsotopaula@gmail.com>
2021-02-08 20:37:56 +0100

    Get search and titles

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,34 @@
+/* eslint-disable quotes */
+/* eslint-disable no-console */
+/* eslint-disable indent */
 'use strict';
 
-console.log('>> Ready :)');
+//console.log('antes de const');
+
+const showList = document.querySelector(".showlist");
+const searchButton = document.querySelector(".js-button");
+//console.log(`después de const`);
+let allShow = [];
+
+function getDataApi(ev) {
+    ev.preventDefault();
+   const nameShowInput = document.querySelector(".js-input");
+   const inputWord = nameShowInput.value;
+   fetch("http://api.tvmaze.com/search/shows?q="+inputWord)
+   .then( response => response.json())
+   // document.body.innerHTML=data[0].score);
+   .then(data =>{
+      let html = "";
+      for (let index = 0; index < data.length; index++) {
+         const element = data[index];
+         html += "<li class = item>"
+         html+= data[index].show.name;
+         html+= "</li>"
+      }
+      showList.innerHTML = html;
+   })
+
+       
+   
+}
+searchButton.addEventListener("click",getDataApi);
