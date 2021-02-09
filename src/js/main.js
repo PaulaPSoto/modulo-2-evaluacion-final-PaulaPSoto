fd3264f887eedc31873b44f7b2c7c31d6300d9aa
PaulaPSoto <paezsotopaula@gmail.com>
2021-02-09 18:32:47 +0100

    Look for repetition

--- src/js/main.js
+++ src/js/main.js
@@ -9,11 +9,8 @@ const showList = document.querySelector(".showlist");
 const searchButton = document.querySelector(".js-button");
 const showfav = document.querySelector(".showfav");
 
-
-
 let allShow = [];
 let favoriteShow = [];
-
 
 //Función para coger del Api
 function getDataApi() {
@@ -21,30 +18,29 @@ function getDataApi() {
    const inputWord = nameShowInput.value;
    fetch("http://api.tvmaze.com/search/shows?q="+inputWord)
    .then( response => response.json())
-   // document.body.innerHTML=data[0].score);
    .then(data =>{
       allShow = data;
       renderShow();
    });
 }
-
+//Para recorrer el array de allShow y añadirle el html
 function renderShow(){
    let htmlShowList = "";
    let myimage = "";
-   for (let index = 0; index < allShow.length-7; index++) {
+   for (let index = 0; index < allShow.length; index++) {
       htmlShowList += "<li class = item>";
-      if (typeof allShow[index].show.image === null ) {
+      if (allShow[index].show.image !== null) {
       console.log ("Soy indefinida");
-      //  myimage = "src="./assets/images/photodefault.png";
+        myimage = `src="./assets/images/photodefault.png"`;
         } else{
-         console.log ("Soy definida");
-         myimage = allShow[index].show.image.medium;
+        console.log ("Soy definida");
+        //myimage = allShow[index].show.image.medium;
         }
         htmlShowList += `<img class="js-imghtml" src=`+ myimage +  ` alt=""></img>`;
         htmlShowList += allShow[index].show.name;
         htmlShowList+= "</li>";
    }
-   showList.innerHTML = htmlShowList;
+    showList.innerHTML = htmlShowList;
    //fav
    //recoger todos li - item
    const getLis = document.querySelectorAll(".item");
@@ -55,37 +51,39 @@ function renderShow(){
     console.log(getLis.length);
   }
 }
-
+//local Storage coger lo guardado y pasarlo a JSON
+function getLocalStorage(){
+   const favLocal = JSON.parse(localStorage.getItem("favorite"));
+   return favLocal;
+}
+//local Storage guardarlo y convertirlo en cadena
 function setLocalStorarage(){
    localStorage.setItem ("favorite",JSON.stringify(favoriteShow));
-  }
+}
 
 
 function handleFav(ev) {
-    console.log('marcado favorito');
      const livalue = ev.target.innerHTML;
-     console.log(ev.target);
-     ev.target.classList.add('favorite');
+     //ev.target.classList.add('favorite');
      favoriteShow.push(livalue);
-       // console.log('hola?');
-        console.log('gg'+favoriteShow);
-      //  console.log(getLis.length);
-      renderFav();
-      setLocalStorarage();
+     renderFav();
+     setLocalStorarage();
 }
 
 
 function renderFav() {
+   let favoriteLocal = getLocalStorage();
     let html ="";
-    for (let index = 0; index < favoriteShow.length; index++) {
+    for (let index = 0; index < favoriteLocal.length; index++) {
        html += "<li class = lifav>";
-       html += favoriteShow[index];
+       html += favoriteLocal[index];
        html += "</li>";
     }
     showfav.innerHTML = html;
-
- //const getLis = document.querySelectorAll(".item");
 }
 
 
+
 searchButton.addEventListener("click",getDataApi);
+renderFav();
+getLocalStorage();
